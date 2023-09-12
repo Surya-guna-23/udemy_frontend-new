01c0a545d02e5a0027e141c7815b115d16c60bfc
suryag23 <surya_gunasekaran@comcast.com>
2023-09-12 15:35:20 +0530

    node

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// var generateName = require('sillyname');
+// import  generateName from"sillyname";
+// var sillyName = generateName();
+// console.log(`my name is ${sillyName} .`);
+
+import superheroes from "superheroes";
+var name = superheroes.random();
+console.log(`I am a Super hero My Name is ${name}`);
